0d0b3bb9fb629312425fb617986d627b744ce547
HaloHan2017 <1006653740@qq.com>
2020-07-23 19:29:36 +0800

    [GuessNumberGame] [jerry] feature: add should_return_1A1B_when_guess_1025_given_1234

--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -5,14 +5,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuessNumberGameTest {
+    private final String answer = "1234";
     @Test
     public void should_return_4A0B_when_guess_1234_given_1234() {
         // given
-        String answer = "1234";
         GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
         // when
         String result = guessNumberGame.guess("1234");
         // then
         assertEquals("4A0B", result);
+    }
+
+    @Test
+    public void should_return_1A1B_when_guess_1025_given_1234() {
+        // given
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        // when
+        String result = guessNumberGame.guess("1234");
+        // then
+        assertEquals("1A1B", result);
     }
 }
